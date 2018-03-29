936779dbde387e887fc2a3e5a94482eedd5208c4
andr68rus <ashtmb@gmail.com>
2018-03-29 13:17:12 +0300

    chiron: remove FP hardcode Lets wrapper choose what to load

--- du.mk
+++ du.mk
@@ -48,6 +48,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.boot.fingerprint=fpc
